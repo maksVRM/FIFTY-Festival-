6f86ba5a544090cdc46b7a9b24757fdd6dbcc9c2
Maksym Radchenko <maksikchipic@gmail.com>
2025-10-24 14:42:27 +0200

    changed animation of circles and squiggles with a help of JS and random numbers

--- fifty.js
+++ fifty.js
@@ -19,3 +19,59 @@ function makeMarquee() {
 
 // here we actually run our makeMarquee function
 makeMarquee()
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+
+// here we grab a;; our the .circles from the html
+const circles = document.querySelectorAll('.circle')
+
+// circles returns us an array so we need to loop through it 
+// inside of the forEach we get access to each individual element
+// along with it's index
+circles.forEach(function (circle, index) {
+
+    // in here we get acces to each one as 'circle'
+    circle.animate([
+        // keyframes
+        { transform: 'scale(1)' },
+        { transform: 'scale(1.2)' },
+        { transform: 'scale(1)' }
+    ], {
+        // timing options
+        // here we use the index to create a staggered animation delay
+        delay: 300 * index,
+        duration: 3000,
+        iterations: Infinity
+    });
+})
+
+
+const squiggles = document.querySelectorAll('.squiggle')
+
+
+
+squiggles.forEach(function (squiggle, index) {
+    // gets a random number between 0 and 45 using our random function
+    // from stackoverflow
+    const randomNumber = random(0, 45)
+    // create a bit of randomness for our animation delay
+    console.log(randomNumber)
+
+
+    squiggle.animate([
+        // keyframes
+        { transform: 'rotate(0)' },
+        // here we join our random number into our rotate property
+        { transform: 'rotate(' + randomNumber + 'deg)' },
+        { transform: 'rotate(0)' }
+    ], {
+        // timing options
+        // here we use the index to create a staggered animation delay
+        delay: 300 * index,
+        duration: 5000,
+        iterations: Infinity
+    });
+})
